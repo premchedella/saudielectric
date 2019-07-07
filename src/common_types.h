6d926109f6d7f5fe0c280182981e33e077797ca7
Prem Kumar Chedella <prem.chedella@gmail.com>
2019-07-07 08:46:22 +0300

    Added u to the Unicode strings, Visual Studio 2017 does not generate proper data.

--- src/common_types.h
+++ src/common_types.h
@@ -61,19 +61,19 @@ const std::vector<int> type_1_non_vat_small_2 = {10, 11, 9, 9, 11, 10, 2, 3 };
 
 //Types
 #define TYPE_AR_INDUSTRIAL u"صناعي"
-#define TYPE_AR_INDUSTRIAL_1 "\uFEBB\uFEE8\uFE8E\uFECB\uFEF2"
+#define TYPE_AR_INDUSTRIAL_1 u"\uFEBB\uFEE8\uFE8E\uFECB\uFEF2"
 #define TYPE_AR_RESIDENTIAL u"سكني"
-#define TYPE_AR_RESIDENTIAL_1 "\uFEB3\uFEDC\uFEE8\uFEF2"
+#define TYPE_AR_RESIDENTIAL_1 u"\uFEB3\uFEDC\uFEE8\uFEF2"
 #define TYPE_AR_COMMERCIAL u"تجاري"
-#define TYPE_AR_COMMERCIAL_1 "\uFE97\uFEA0\uFE8E\uFEAD\uFEF1"
+#define TYPE_AR_COMMERCIAL_1 u"\uFE97\uFEA0\uFE8E\uFEAD\uFEF1"
 #define TYPE_AR_MEDICAL u"خيري"
-#define TYPE_AR_MEDICAL_1 "\uFEBB\uFEA4\uFEF2"
+#define TYPE_AR_MEDICAL_1 u"\uFEBB\uFEA4\uFEF2"
 #define TYPE_AR_CHARITY u"تعليمي"
-#define TYPE_AR_CHARITY_1 "\uFEA7\uFEF4\uFEAE\uFEF1"
+#define TYPE_AR_CHARITY_1 u"\uFEA7\uFEF4\uFEAE\uFEF1"
 #define TYPE_AR_EDUCATIONAL u"تعليمي"
-#define TYPE_AR_EDUCATIONAL_1 "\uFE97\uFECC\uFEE0\uFEF4\uFEE4\uFEF2"
+#define TYPE_AR_EDUCATIONAL_1 u"\uFE97\uFECC\uFEE0\uFEF4\uFEE4\uFEF2"
 #define TYPE_AR_GOVERNMENTAL u"حكومي"
-#define TYPE_AR_GOVERNMENTAL_1 "\uFEA3\uFEDC\uFEEE\uFEE3\uFEF2"
+#define TYPE_AR_GOVERNMENTAL_1 u"\uFEA3\uFEDC\uFEEE\uFEE3\uFEF2"
 
 
 
@@ -86,8 +86,8 @@ const std::vector<int> type_1_non_vat_small_2 = {10, 11, 9, 9, 11, 10, 2, 3 };
 #define TYPE_ENG_GOVERNMENTAL "Governmental"
 
 
-#define TOTAL_INNOVICES_1 "عدد الفواتير"
-#define TOTAL_INNOVICES "\uFE8D\uFEDF\uFED4\uFEEE\uFE8D\uFE97\uFEF4\uFEAE"
+#define TOTAL_INNOVICES_1 u"عدد الفواتير"
+#define TOTAL_INNOVICES u"\uFE8D\uFEDF\uFED4\uFEEE\uFE8D\uFE97\uFEF4\uFEAE"
 
 #endif //COMMON_TYPES_H_
 
