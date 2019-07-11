5c8df61b3b4bbb0a813e9108683d97332f694472
Prem Kumar Chedella <prem.chedella@gmail.com>
2019-07-11 09:03:17 +0300

    Removed un-necessary constants in the common types header file.

--- src/common_types.h
+++ src/common_types.h
@@ -13,50 +13,11 @@ typedef std::vector<QString> Line;
 
 #define START_BLOCK_INDEX 0
 #define END_BLOCK_INDEX 14
+
 #define START_BLOCK_WORD u"ﺭﻗﻢ"
 #define END_BLOCK_WORD u"ﺍﻟﻘﺪﺭﺓ‪:‬‬"
 #define LAST_BLOCK_WORD u"ﻣﺠﻤﻮﻉ"
-
 #define START_BLOCK_WORD_TWO u"ﺍﻟﻔﺌﺔ\\"
-
-#define INDUSTRIAL_BLOCK_LENGTH 36
-#define RESIDENTIAL_BLOCK_LENGTH 9
-
-#define RESIDENTIAL_BLOCK_LENGTH_VAT 9
-#define RESIDENTIAL_BLOCK_LENGTH_NON_VAT 8
-#define RESIDENTIAL_BLOCK_LENGTH_PARTIAL_VAT 15
-
-#define TYPE_1_NON_VAT_SMALL_SIZE 8
-const std::vector<int> type_1_non_vat_small_1 = {10, 11, 9, 9, 12, 12, 2, 3};
-const std::vector<int> type_1_non_vat_small_2 = {10, 11, 9, 9, 11, 10, 2, 3 };
-
-#define TYPE_5_BLOCK_LENGTH_VAT 15
-#define TYPE_5_BLOCK_LENGTH_NON_VAT 14
-#define TYPE_5_BLOCK_LENGTH_NON_VAT_ANOTHER 19
-#define TYPE_5_BLOCK_LENGTH_PARTIAL_VAT 16
-
-#define TYPE_1_BLOCK_LENGTH_VAT 22
-#define TYPE_1_BLOCK_LENGTH_NON_VAT 21
-#define TYPE_1_BLOCK_LENGTH_PARTIAL_VAT 28
-
-#define TYPE_1_LINE_5_RESIDENTIAL 12
-
-#define HARDLES_WORDS_SIZE 16
-#define HARDLES_WORD_1_POSITION 12
-#define HARDLES_WORD_2_POSITION 14
-#define HARDLES_NO_LINES 6
-
-#define INDUSTRY_HARDLES_WORDS_SIZE 1
-#define INDUSTRY_HARDLES_WORD_POSITION 0
-#define INDUSTRY_HARDLES_VALUE 0
-
-
-//Data Base types
-#define DATABASE_DIR_NAME "database"
-#define DATABASE_NAME "data.db"
-#define MAIN_TABLE_NAME "master"
-#define SUB_TABLE_START "sub_"
-
 
 //Types
 #define TYPE_AR_INDUSTRIAL u"صناعي"
@@ -74,8 +35,6 @@ const std::vector<int> type_1_non_vat_small_2 = {10, 11, 9, 9, 11, 10, 2, 3 };
 #define TYPE_AR_GOVERNMENTAL u"حكومي"
 #define TYPE_AR_GOVERNMENTAL_1 u"\uFEA3\uFEDC\uFEEE\uFEE3\uFEF2"
 
-
-
 #define TYPE_ENG_INDUSTRIAL "Industrial"
 #define TYPE_ENG_RESIDENTIAL "Residential"
 #define TYPE_ENG_COMMERCIAL "Commercial"
@@ -84,9 +43,10 @@ const std::vector<int> type_1_non_vat_small_2 = {10, 11, 9, 9, 11, 10, 2, 3 };
 #define TYPE_ENG_EDUCATIONAL "Educational"
 #define TYPE_ENG_GOVERNMENTAL "Governmental"
 
-
+//Total Invoices
 #define TOTAL_INNOVICES_1 u"عدد الفواتير"
 #define TOTAL_INNOVICES u"\uFE8D\uFEDF\uFED4\uFEEE\uFE8D\uFE97\uFEF4\uFEAE"
+
 
 #endif //COMMON_TYPES_H_
 
