9894c82bee4171d8698ff3f5fe6ad51d8e185e4b
Prem Kumar Chedella <prem.chedella@gmail.com>
2020-11-04 12:51:29 +0300

    SEC:Parser1 big: Parse Line 1 Data.

--- src/parser_1_big.cc
+++ src/parser_1_big.cc
@@ -17,6 +17,20 @@ void Parser1Big::Parse(Block data_in, AccountDetails* acc_details)
 {
   std::cout << "Block Size = " << data_in.size() << std::endl;
 
-  QStringList line_data;
+  QStringList line_data;  
+  
+  // Line Number is 1
+  int line_no = 0;
+  std::cout << "Parse Line " << line_no + 1 << ":: ";
+  try
+  {
+    line_data = data_in.at(line_no);
+    Parser1Lines::Line1(line_data, acc_details);
+  }
+  catch (...)
+  {
+    acc_details->parsing_ = "Partial";
+    acc_details->reason_ += "No Line 1;";
+  }
   
 }
