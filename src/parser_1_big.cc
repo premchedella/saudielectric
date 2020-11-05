c1fb22af3a3ea69982f3a267a156da17681851ab
Prem Kumar Chedella <prem.chedella@gmail.com>
2020-11-05 14:08:58 +0300

    SEC:Parser1 big: Parse Line 11 Data.

--- src/parser_1_big.cc
+++ src/parser_1_big.cc
@@ -132,4 +132,18 @@ void Parser1Big::Parse(Block data_in, AccountDetails* acc_details)
     acc_details->parsing_ = "Partial";
     acc_details->reason_ += "No Line 10;";
   }
+
+  //Line No. 11
+  line_no++;
+  std::cout << "Parse Line " << line_no + 1 << ":: ";
+  try
+  {
+    line_data = data_in.at(line_no);
+    Parser1Lines::Line10(line_data, acc_details);
+  }
+  catch (...)
+  {
+    acc_details->parsing_ = "Partial";
+    acc_details->reason_ += "No Line 11;";
+  }
 }
