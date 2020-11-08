2036347ca7a5ade0b6b6d95a44f3a09437783fd4
Prem Kumar Chedella <prem.chedella@gmail.com>
2020-11-08 11:00:51 +0300

    SEC:Parser1 big: Call Parser13Big function.

--- src/parser_1_big.cc
+++ src/parser_1_big.cc
@@ -210,7 +210,7 @@ void Parser1Big::Parse(Block data_in, AccountDetails* acc_details)
   try
   {
     line_data = data_in.at(line_no);
-    Parser1Lines::Line13(line_data, acc_details);
+    Parser1Lines::Line13Big(line_data, acc_details);
   }
   catch (...)
   {
